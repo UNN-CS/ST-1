27cffe701d2b8ece4943fceb9d2b974b9c7b56af
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-03-03 01:42:35 +0300

    Library impl + sample program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
 #include "alg.h"
-
 int main() {
+    int64_t input = 0;
+    std::cout << "Enter integer to check if it's a prime number: ";
+    std::cin >> input;
+    std::cout << "\"" << input << "\" "
+        "is " << (checkPrime(input) ? "" : "not ") << "a prime number\n";
     return 0;
 }
