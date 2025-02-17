ef4735629702b28064fd871b0e00a73dd36dd930
odincovm <112872592+odincovm@users.noreply.github.com>
2025-02-17 20:45:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,40 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+	if (value < 2)
+		return false;
+	for (uint64_t i = 2;i < value;i++) {
+		if (value % i == 0)
+			return false;
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+	count = 0;
+	for (uint64_t i = 2; i < n;i++) {
+		if (checkPrime(i))
+			count++
+		if (count == n) 
+			return i	
+	}
+	return -1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+	uint64_t i = value + 1;
+	while (i< std::numeric_limits<uint64_t>::max())
+		if (checkPrime(i))
+			return i;
+		i++;
+	return -1;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+	uint64_t sum = 0;
+	for (uint64_t i = 2;i < hbound;i++) {
+		if (checkPrime(i))
+			sum += i;
+	}
+	return sum;
 }
