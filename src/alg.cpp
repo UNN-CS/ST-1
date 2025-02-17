b003708b62315621304fd8e125c1d07b2b5f49db
Gn4ik <112872726+Gn4ik@users.noreply.github.com>
2025-02-17 21:23:01 +0300

    Add functions

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,34 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  for(int i = 2; i <= sqrt(value); i++)
+    if(value % i == 0)
+      return false;
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t res = 2;
+  n--;
+  while (n > 0) {
+    res++;
+    if(checkPrime(res)) n--;
+  }
+  return res;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t res = value;
+  while(true) {
+    res++;
+    if(checkPrime(res)) break;
+  }
+  return res;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for(uint64_t i = 2; i < hbound; i++)
+    if(checkPrime(i)) sum += i;
+  return sum;
 }
