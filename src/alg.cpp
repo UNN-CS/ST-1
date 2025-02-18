6db1cc3a99ecb685d96a935a0f29e5a70f1fbdba
ovVrLFg8ks <122876910+ovVrLFg8ks@users.noreply.github.com>
2025-02-18 21:32:35 +0300

    weirder indentation implementation

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,12 +23,12 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   while (!checkPrime(++value)) {}
-  return value;
-}
+    return value;
+  }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-  for (uint64_t i = 2; i < hbound; i++)
-  if (checkPrime(i)) sum += i;
-  return sum;
-}
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++)
+    if (checkPrime(i)) sum += i;
+    return sum;
+  }
