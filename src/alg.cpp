a16b31523d3217e9655efebd2cf466342f6c079a
Andrey <andreycol75@gmail.com>
2025-02-19 18:15:04 +0300

    fix checker

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,38 +1,38 @@
 // Copyright 2025 UNN-CS
-#include "alg.h"
 #include <cstdint>
+#include "alg.h"
 
 bool checkPrime(uint64_t value) {
   if (value < 2)
-    return false;
+  return false;
   if (value == 2 || value == 3)
-    return true;
+  return true;
   if (value % 2 == 0 || value % 3 == 0)
-    return false;
+  return false;
 
   for (uint64_t i = 5; i * i <= value; i += 6) {
     if (value % i == 0 || value % (i + 2) == 0)
-      return false;
+    return false;
   }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
   if (n == 0)
-    return 0;
+  return 0;
 
   uint64_t count = 0, num = 1;
   while (count < n) {
     num++;
     if (checkPrime(num))
-      count++;
+    count++;
   }
   return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
   if (value < 2)
-    return 2;
+  return 2;
 
   do {
     value++;
@@ -45,7 +45,7 @@ uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i))
-      sum += i;
+    sum += i;
   }
   return sum;
 }
