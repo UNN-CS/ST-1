9f7991f508775f20b01e1dfbc972f5b4699e2a9f
Ksu04 <xenia.i4etovkina@yandex.ru>
2025-02-20 23:33:59 +0300

    v1

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,41 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  for (int i = 2; i < (int)(value ** (1/2)) + 1; i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  int counter = 0;
+  int num = 2;
+  while (counter != n) {
+    if (checkPrime(num)) {
+      counter++;
+    } 
+    num++;
+  }
+  return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  int num = value + 1;
+  while (!checkPrime(num)) {
+    num++;
+  }
+  return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  int s = 0;
+  int num = 2;
+  for (int i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      s += i;
+    }
+  }
+  return s;
 }
