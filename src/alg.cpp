c30489eca9981314c627ff311568d84717beb1e9
DSFKnight <Gran122004@gmail.com>
2025-02-20 16:50:28 +0300

    Format fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,8 +8,8 @@ bool checkPrime(uint64_t value) {
   if (value == 2 || value == 3) return true;
   if (value % 2 == 0) return false;
   for (uint64_t i = 3; i * i <= value; i += 2) {
-	if (value % i == 0) { 
-	  return false; 
+	if (value % i == 0) {
+	  return false;
 	}
   }
   return true;
@@ -23,8 +23,8 @@ uint64_t nPrime(uint64_t n) {
     current++;
 	if (checkPrime(current)) {
 	  count++;
-	  if (count == n) { 
-	    return current; 
+	  if (count == n) {
+	    return current;
 	  }
 	}
   }
@@ -33,8 +33,8 @@ uint64_t nPrime(uint64_t n) {
 uint64_t nextPrime(uint64_t value) {
   uint64_t candidate = value + 1;
   while (true) {
-	if (checkPrime(candidate)) { 
-	 return candidate; 
+	if (checkPrime(candidate)) {
+	 return candidate;
 	}
 	candidate++;
   }
@@ -43,8 +43,8 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
-	if (checkPrime(i)) { 
-	  sum += 1; 
+	if (checkPrime(i)) {
+	  sum += 1;
 	}
   }
   return sum;
