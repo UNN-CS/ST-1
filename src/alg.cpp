b20d52b13156835f453e287b1e02962c079a107b
shyravina19102004 <shyravina19102004@gmail.com>
2025-02-20 22:38:29 +0300

    d

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,7 +10,7 @@ bool checkPrime(uint64_t value) {
 
     for (uint64_t i = 5; i * i <= value; i += 6) {
     if (value % i == 0 || value % (i + 2) == 0)
-        return false;
+    return false;
   }
   return true;
 }
