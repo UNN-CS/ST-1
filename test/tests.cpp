6d371e69e6d07d98f90e21f291a153477f4dbcd7
Ksu04 <xenia.i4etovkina@yandex.ru>
2025-02-20 23:47:07 +0300

    v4

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,93 +6,87 @@
 
 
 
-TEST(st1, checkPrime1) {
+TEST(prime_num, checkPrime1) {
   EXPECT_EQ(checkPrime(2), true);
 }
 
-TEST(st1, checkPrime2) {
+TEST(prime_num, checkPrime2) {
   EXPECT_EQ(checkPrime(4), false);
 }
 
-TEST(st1, checkPrime3) {
+TEST(prime_num, checkPrime3) {
   EXPECT_EQ(checkPrime(5), true);
 }
 
-TEST(st1, checkPrime4) {
+TEST(prime_num, checkPrime4) {
   EXPECT_EQ(checkPrime(9929), true);
 }
 
-TEST(st1, checkPrime5) {
+TEST(prime_num, checkPrime5) {
   EXPECT_EQ(checkPrime(9999), false);
 }
 
-TEST(st1, nPrime1) {
+TEST(prime_num, nPrime1) {
   uint64_t num = 6;
   uint64_t expected = 13;
   EXPECT_EQ(expected, nPrime(num));
 }
 
-TEST(st1, nPrime2) {
+TEST(prime_num, nPrime2) {
   uint64_t num = 6;
   uint64_t expected = 13;
   EXPECT_EQ(expected, nPrime(num));
 }
 
-TEST(st1, nPrime3) {
+TEST(prime_num, nPrime3) {
   uint64_t num = 12;
   uint64_t expected = 37;
   EXPECT_EQ(expected, nPrime(num));
 }
 
-TEST(st1, nPrime4) {
+TEST(prime_num, nPrime4) {
   uint64_t num = 1;
   uint64_t expected = 2;
   EXPECT_EQ(expected, nPrime(num));
 }
 
-TEST(st1, nPrime5) {
+TEST(prime_num, nPrime5) {
   uint64_t num = 6;
   uint64_t expected = 13;
   EXPECT_EQ(expected, nPrime(num));
 }
 
-TEST(st1, nextPrime1) {
+TEST(prime_num, nextPrime1) {
   uint64_t num = 13;
   uint64_t expected = 17;
   EXPECT_EQ(expected, nextPrime(num));
 }
 
-TEST(st1, nextPrime1) {
+TEST(prime_num, nextPrime2) {
   uint64_t num = 5;
   uint64_t expected = 7;
   EXPECT_EQ(expected, nextPrime(num));
 }
 
-TEST(st1, nextPrime2) {
-  uint64_t num = 13;
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, nextPrime(num));
-}
-
-TEST(st1, nextPrime3) {
+TEST(prime_num, nextPrime3) {
   uint64_t num = 12;
   uint64_t expected = 13;
   EXPECT_EQ(expected, nextPrime(num));
 }
 
-TEST(st1, sumPrime1) {
+TEST(prime_num, sumPrime1) {
   uint64_t res = sumPrime(10);
   uint64_t expected = 17;
   EXPECT_EQ(expected, res);
 }
 
-TEST(st1, sumPrime2) {
+TEST(prime_num, sumPrime2) {
   uint64_t res = sumPrime(13);
   uint64_t expected = 28;
   EXPECT_EQ(expected, res);
 }
 
-TEST(st1, sumPrime3) {
+TEST(prime_num, sumPrime3) {
   uint64_t res = sumPrime(39);
   uint64_t expected = 197;
   EXPECT_EQ(expected, res);
