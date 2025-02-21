fa0087222d637a9f5ba61c93276905ae83c974b4
VlJulia <129722951+VlJulia@users.noreply.github.com>
2025-02-21 17:16:57 +0300

    format3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,11 @@
 // Copyright 2025 UNN-CS
 #include "alg.h"
-
+#include <cstdint>
 #include <algorithm>
 #include <cmath>
-#include <cstdint>
 #include <vector>
+
+
 std::vector<uint64_t> primes = {2, 3, 5};
 uint64_t last_checked = 6;
 void newPrimes(uint64_t n) {
@@ -12,17 +13,19 @@ void newPrimes(uint64_t n) {
   if (n <= last_checked) return;
   std::vector<uint64_t> tmp = std::vector<uint64_t>(n, 1);
 
-  for (int i = 2; i < sqrt(n) + 1; i++)  if (tmp[i] != 0)  {
-    for (int j = i * i; j < n; j += i) tmp[j] = 0;
-  }
+  for (int i = 2; i < sqrt(n) + 1; i++)
+    if (tmp[i] != 0) {
+      for (int j = i * i; j < n; j += i) tmp[j] = 0;
+    }
   if (n > 2) {
     tmp[0] = 0;
     tmp[1] = 0;
     tmp[2] = 1;
   }
-  for (int i = last_checked; i < n; i++)  if (tmp[i] == 1) {
-    primes.push_back(i);
-  }
+  for (int i = last_checked; i < n; i++)
+    if (tmp[i] == 1) {
+      primes.push_back(i);
+    }
   last_checked = n;
 }
 
@@ -48,15 +51,17 @@ uint64_t nextPrime(uint64_t value) {
   uint64_t s = e / 2;
   while (primes[s] != value) {
     if (e - b == 1) return primes[e];
-    if (value > primes[s])  b = s;
-    else  e = s;
+    if (value > primes[s])
+      b = s;
+    else
+      e = s;
     s = (b + e) / 2;
   }
   return primes[s + 1];
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  while (primes[primes.size() - 1] < hbound)  {
+  while (primes[primes.size() - 1] < hbound) {
     newPrimes(last_checked + hbound * 5);
   }
   uint64_t sm = 0;
