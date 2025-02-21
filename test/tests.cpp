f36f86c397ff3229569a7e9174127c35a8564823
Ilya2643git <114603462+Ilya2643@users.noreply.github.com>
2025-02-21 15:57:32 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,13 +5,51 @@
 #include "alg.h"
 
 
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(CheckPrimeTest, PrimeNumbers) {
+  EXPECT_TRUE(checkPrime(2));
+  EXPECT_TRUE(checkPrime(3));
+  EXPECT_TRUE(checkPrime(5));
+  EXPECT_TRUE(checkPrime(7));
+  EXPECT_TRUE(checkPrime(11));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(CheckPrimeTest, NonPrimeNumbers) {
+  EXPECT_FALSE(checkPrime(0));
+  EXPECT_FALSE(checkPrime(1));
+  EXPECT_FALSE(checkPrime(4));
+  EXPECT_FALSE(checkPrime(6));
+  EXPECT_FALSE(checkPrime(9));
 }
+
+TEST(NthPrimeTest, FirstPrime) {
+  EXPECT_EQ(nPrime(1), 2);
+}
+
+TEST(NthPrimeTest, SecondPrime) {
+  EXPECT_EQ(nPrime(2), 3);
+}
+
+TEST(NthPrimeTest, TenthPrime) {
+  EXPECT_EQ(nPrime(10), 29);
+}
+
+TEST(NextPrimeTest, After2) {
+  EXPECT_EQ(nextPrime(2), 3);
+}
+
+TEST(NextPrimeTest, After14) {
+  EXPECT_EQ(nextPrime(14), 17);
+}
+
+TEST(NextPrimeTest, After17) {
+  EXPECT_EQ(nextPrime(17), 19);
+}
+
+TEST(SumPrimeTest, SumBelow10) {
+  EXPECT_EQ(sumPrime(10), 17);
+}
+
+TEST(SumPrimeTest, SumBelow20) {
+  EXPECT_EQ(sumPrime(20), 77);
+}
+
