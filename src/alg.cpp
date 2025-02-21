22c43abf708593664f3cbb8dda65ee5cb31cb65b
Cufeed <gusman3000@mail.ru>
2025-02-21 17:45:05 +0300

    b35_fix5

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,11 +6,11 @@ bool checkPrime(uint64_t value) {
     if (value <= 1) {
         return false;
     }
-    if (value == 2) { 
+    if (value == 2) {
         return true;
     }
-    if (value % 2 == 0) { 
-        return false; 
+    if (value % 2 == 0) {
+        return false;
     }
     for (uint64_t i = 3; i * i <= value; i += 2) {
         if (value % i == 0) {
