09c4eb3f3bea890d4a763f10ee8012e45a19eb98
Иван Садиков <sadikov.04@list.ru>
2025-02-21 14:36:42 +0300

    changes2

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,42 +1,42 @@
 // Copyright 2025 UNN-CS
 
-#include <cstdint>
 #include <gtest/gtest.h>
+#include <cstdint>
 
 #include "alg.h"
 
-Test(prime_num, isPrime1) { EXPECT_EQ(checkPrime(19), true); }
+TEST(prime_num, isPrime1) { EXPECT_EQ(checkPrime(19), true); }
 
-Test(prime_num, isPrime2) { EXPECT_EQ(checkPrime(20), false); }
+TEST(prime_num, isPrime2) { EXPECT_EQ(checkPrime(20), false); }
 
-Test(prime_num, isPrime3) { EXPECT_EQ(checkPrime(113), true); }
+TEST(prime_num, isPrime3) { EXPECT_EQ(checkPrime(113), true); }
 
-Test(st1, nprime1) {
+TEST(st1, nprime1) {
   constexpr auto nprime = 11;
   EXPECT_EQ(nPrime(nprime), 5);
 }
 
-Test(prime_num, nprime2) {
+TEST(prime_num, nprime2) {
   constexpr auto nprime = 17;
   EXPECT_EQ(nPrime(17), 7);
 }
 
-Test(prime_num, nextPrime1) {
+TEST(prime_num, nextPrime1) {
   constexpr auto nextprime = 29;
   EXPECT_EQ(nPrime(nextprime), 31);
 }
 
-Test(prime_num, nextPrime2) {
+TEST(prime_num, nextPrime2) {
   constexpr auto nextprime = 67;
   EXPECT_EQ(nPrime(nextprime), 71);
 }
 
-Test(prime_num, sumPrime1) {
+TEST(prime_num, sumPrime1) {
   constexpr auto nbound = 3;
   EXPECT_EQ(sumPrime(nbound), 10);
 }
 
-Test(prime_num, sumPrime2) {
+TEST(prime_num, sumPrime2) {
   constexpr auto nbound = 6;
   EXPECT_EQ(sumPrime(nbound), 41);
 }
