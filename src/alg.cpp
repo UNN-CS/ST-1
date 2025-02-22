21586204806d5501486169025d2572800d4e9e4f
Alexander <87474596+kobych@users.noreply.github.com>
2025-02-23 00:25:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,17 +5,16 @@
 #include <cmath>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  if (value < 2) return false;
+    if (value < 2) return false;
     for (uint64_t i = 2; i <= std::sqrt(value); ++i) {
         if (value % i == 0) return false;
     }
-  return true;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  uint64_t count = 0;
+    uint64_t count = 0;
     uint64_t num = 2;
     while (count < n) {
         if (checkPrime(num)) {
@@ -27,7 +26,7 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t num = value + 1;
+    uint64_t num = value + 1;
     while (true) {
         if (checkPrime(num)) {
             return num;
@@ -37,7 +36,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
+    uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; ++i) {
         if (checkPrime(i)) {
             sum += i;
