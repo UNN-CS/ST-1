29b05008d7c38cc609ed7a0eb808888d9948e266
KAPUSHA228 <shura_tsatsyn_2003@mail.ru>
2025-02-22 14:06:03 +0300

    minifix

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,29 +5,29 @@
 
 bool checkPrime(uint64_t value) {
     if (value <= 1) {
-        return false;
-    }
+    return false;
+  }
     for (uint64_t i = 2; i * i <= value; ++i) {
         if (value % i == 0) {
-            return false;
-        }
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) return 0;
+  if (n == 0) return 0;
 
-    uint64_t count = 0;
-    uint64_t num = 2;
+  uint64_t count = 0;
+  uint64_t num = 2;
 
     while (count < n) {
         if (checkPrime(num)) {
-            count++;
-        }
-        if (count < n)
-            num++;
+      count++;
     }
+    if (count < n)
+    num++;
+  }
 
     return num;
 }
@@ -37,17 +37,17 @@ uint64_t nextPrime(uint64_t value) {
     while (true) {
         if (checkPrime(num)) {
             return num;
-        }
-        num++;
     }
+    num++;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
+  uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; ++i) {
         if (checkPrime(i)) {
-            sum += i;
-        }
+      sum += i;
     }
-    return sum;
+  }
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -16,53 +16,53 @@ TEST(st1, sumPrime2) {
   EXPECT_EQ(expected, res);
 }
 TEST(st1, checkPrime_basic1) {
-    EXPECT_FALSE(checkPrime(1));
+  EXPECT_FALSE(checkPrime(1));
 }
 TEST(st1, checkPrime_basic2) {
-    EXPECT_TRUE(checkPrime(2));
+  EXPECT_TRUE(checkPrime(2));
 }
 TEST(st1, checkPrime_basic3) {
-    EXPECT_TRUE(checkPrime(3));
+  EXPECT_TRUE(checkPrime(3));
 }
 TEST(st1, checkPrime_basic4) {
-    EXPECT_FALSE(checkPrime(4));
+  EXPECT_FALSE(checkPrime(4));
 }
 TEST(st1, checkPrime_basic5) {
-    EXPECT_TRUE(checkPrime(5));
+  EXPECT_TRUE(checkPrime(5));
 }
 TEST(st1, checkPrime_basic6) {
-    EXPECT_FALSE(checkPrime(6));
+  EXPECT_FALSE(checkPrime(6));
 }
 TEST(st1, checkPrime_basic7) {
-    EXPECT_TRUE(checkPrime(7));
+  EXPECT_TRUE(checkPrime(7));
 }
 TEST(PrimeTest, nextPrime_basic1) {
-    EXPECT_EQ(2, nextPrime(1));
+  EXPECT_EQ(2, nextPrime(1));
 } 
 TEST(PrimeTest, nextPrime_basic2) {
-    EXPECT_EQ(3, nextPrime(2));
+  EXPECT_EQ(3, nextPrime(2));
 }  
 TEST(PrimeTest, nextPrime_basic3) {
-    EXPECT_EQ(5, nextPrime(3));
+  EXPECT_EQ(5, nextPrime(3));
 } 
 TEST(PrimeTest, nextPrime_basic4) {
-    EXPECT_EQ(5, nextPrime(4));
+  EXPECT_EQ(5, nextPrime(4));
 }
 TEST(PrimeTest, nextPrime_basic5) {
-    EXPECT_EQ(7, nextPrime(5));
+  EXPECT_EQ(7, nextPrime(5));
 }
 TEST(PrimeTest, nextPrime_basic6) {
-    EXPECT_EQ(7, nextPrime(6));
+  EXPECT_EQ(7, nextPrime(6));
 }
 TEST(PrimeTest, nextPrime_basic7) {
-    EXPECT_EQ(11, nextPrime(7));
+  EXPECT_EQ(11, nextPrime(7));
 }
 TEST(PrimeTest, nextPrime_basic8) {
-    EXPECT_EQ(11, nextPrime(8));
+  EXPECT_EQ(11, nextPrime(8));
 }
 TEST(PrimeTest, nextPrime_basic9) {
-    EXPECT_EQ(11, nextPrime(9));
+  EXPECT_EQ(11, nextPrime(9));
 }
 TEST(PrimeTest, nextPrime_basic10) {
-    EXPECT_EQ(11, nextPrime(10));
+  EXPECT_EQ(11, nextPrime(10));
 }
