addc511318eaad250a838671e27c088a8a7b9455
tyurinm <112872782+tyurinm@users.noreply.github.com>
2025-02-22 17:23:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,34 +1,34 @@
 // Copyright 2025 UNN-CS
 #include <cstdint>
-#include "alg.h"
 #include <cmath>
+#include "alg.h"
 
 bool checkPrime(uint64_t value) {
   if (value <= 1) return false;
   if (value == 2) return true;
   if (value % 2 == 0) return false;
-  
-  uint64_t limit = std::sqrt(value); 
+
+  uint64_t limit = std::sqrt(value);
   for (uint64_t i = 3; i <= limit; i += 2) {
     if (value % i == 0) return false;
   }
-  
+
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
   if (n == 1) return 2;
-  uint64_t count = 1; 
+  uint64_t count = 1;
   uint64_t num = 3;
-  
+
   while (count < n) {
     if (checkPrime(num)) {
       count++;
     }
-    num += 2; 
+    num += 2;
   }
-  
-  return num - 2; 
+
+  return num - 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
