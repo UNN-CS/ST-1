5754ac2f41b330509627396ee1af90d12e9d940b
tyurinm <112872782+tyurinm@users.noreply.github.com>
2025-02-22 17:16:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,12 +7,12 @@ bool checkPrime(uint64_t value) {
   if (value <= 1) return false;
   if (value == 2) return true;
   if (value % 2 == 0) return false;
-
+  
   uint64_t limit = std::sqrt(value); 
   for (uint64_t i = 3; i <= limit; i += 2) {
     if (value % i == 0) return false;
   }
-
+  
   return true;
 }
 
@@ -20,14 +20,14 @@ uint64_t nPrime(uint64_t n) {
   if (n == 1) return 2;
   uint64_t count = 1; 
   uint64_t num = 3;
-    
+  
   while (count < n) {
     if (checkPrime(num)) {
       count++;
     }
     num += 2; 
   }
-
+  
   return num - 2; 
 }
 
