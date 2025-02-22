f9e95c135eb2cddbe41edac1bfce4674f1c7e984
xinod2000 <49196752+xinod2000@users.noreply.github.com>
2025-02-22 22:55:42 +0300

    Add algoritm

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,12 +4,6 @@
 #include <cstdint>
 #include "alg.h"
 
-
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
-}
 TEST(st1, sumPrime2) {
   uint64_t res = sumPrime(10);
   uint64_t expected = 17;
