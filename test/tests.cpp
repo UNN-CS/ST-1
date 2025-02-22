1fefb175f40d77cc779e44891a5b057d7a49eaf2
tyurinm <112872782+tyurinm@users.noreply.github.com>
2025-02-22 17:17:55 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,27 +5,27 @@
 #include "alg.h"
 
 TEST(CheckPrimeTest, ValueZero) {
-  EXPECT_FALSE(checkPrime(0));  
+  EXPECT_FALSE(checkPrime(0));
 }
 
 TEST(CheckPrimeTest, ValueFive) {
-  EXPECT_TRUE(checkPrime(5)); 
+  EXPECT_TRUE(checkPrime(5));
 }
 
 TEST(CheckPrimeTest, ValueTwentyOne) {
-  EXPECT_FALSE(checkPrime(21)); 
+  EXPECT_FALSE(checkPrime(21));
 }
 
 TEST(CheckPrimeTest, ValueFiftyThree) {
-  EXPECT_TRUE(checkPrime(53));  
+  EXPECT_TRUE(checkPrime(53));
 }
 
 TEST(CheckPrimeTest, ValueHundred) {
-  EXPECT_FALSE(checkPrime(100)); 
+  EXPECT_FALSE(checkPrime(100));
 }
 
 TEST(CheckPrimeTest, LargePrimeTest) {
-  EXPECT_TRUE(checkPrime(1000000007)); 
+  EXPECT_TRUE(checkPrime(1000000007));
 }
 
 TEST(CheckPrimeTest, LargeNonPrimeTest) {
@@ -33,39 +33,39 @@ TEST(CheckPrimeTest, LargeNonPrimeTest) {
 }
 
 TEST(NPrimeTest, ThirdPrime) {
-  EXPECT_EQ(nPrime(3), 5); 
+  EXPECT_EQ(nPrime(3), 5);
 }
 
 TEST(NPrimeTest, SeventhPrime) {
-  EXPECT_EQ(nPrime(7), 17); 
+  EXPECT_EQ(nPrime(7), 17);
 }
 
 TEST(NPrimeTest, FifteenthPrime) {
-  EXPECT_EQ(nPrime(15), 47); 
+  EXPECT_EQ(nPrime(15), 47);
 }
 
 TEST(NPrimeTest, TwentiethPrime) {
-  EXPECT_EQ(nPrime(20), 71); 
+  EXPECT_EQ(nPrime(20), 71);
 }
 
 TEST(NPrimeTest, ThirtySecondPrime) {
-  EXPECT_EQ(nPrime(32), 131); 
+  EXPECT_EQ(nPrime(32), 131);
 }
 
 TEST(NextPrimeTest, AfterTwentyFive) {
-  EXPECT_EQ(nextPrime(25), 29); 
+  EXPECT_EQ(nextPrime(25), 29);
 }
 
 TEST(NextPrimeTest, AfterSixty) {
-  EXPECT_EQ(nextPrime(60), 61); 
+  EXPECT_EQ(nextPrime(60), 61);
 }
 
 TEST(NextPrimeTest, AfterNinetyEight) {
-  EXPECT_EQ(nextPrime(98), 101); 
+  EXPECT_EQ(nextPrime(98), 101);
 }
 
 TEST(NextPrimeTest, AfterHundredAndFiftyThree) {
-  EXPECT_EQ(nextPrime(153), 157); 
+  EXPECT_EQ(nextPrime(153), 157);
 }
 
 TEST(NextPrimeTest, AfterTwoHundred) {
@@ -73,11 +73,11 @@ TEST(NextPrimeTest, AfterTwoHundred) {
 }
 
 TEST(SumPrimeTest, SumUpToFifty) {
-  EXPECT_EQ(sumPrime(50), 328);  
+  EXPECT_EQ(sumPrime(50), 328);
 }
 
 TEST(SumPrimeTest, SumUpToHundred) {
-  EXPECT_EQ(sumPrime(100), 1060); 
+  EXPECT_EQ(sumPrime(100), 1060);
 }
 
 TEST(SumPrimeTest, SumUpToThirty) {
@@ -85,7 +85,7 @@ TEST(SumPrimeTest, SumUpToThirty) {
 }
 
 TEST(SumPrimeTest, SumUpToSeventyFive) {
-  EXPECT_EQ(sumPrime(75), 1137); 
+  EXPECT_EQ(sumPrime(75), 712);
 }
 
 TEST(SumPrimeTest, SumUpToFiveHundred) {
@@ -93,5 +93,5 @@ TEST(SumPrimeTest, SumUpToFiveHundred) {
 }
 
 TEST(SumPrimeTest, SumUpToOneMillion) {
-  EXPECT_EQ(sumPrime(1000000), 37550402023); 
+  EXPECT_EQ(sumPrime(1000000), 37550402023);
 }
