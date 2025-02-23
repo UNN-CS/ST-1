73fb501f9eb6ed1686368625b0dec6c301b557bc
KADCHDR <125730153+KADCHDR@users.noreply.github.com>
2025-02-23 14:48:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,16 @@
 // Copyright 2025 UNN-CS
-#include <cstdint>
-#include "alg.h"
-
+#include <cmath>
+#include <vector>
+#include <algorithm>
 
 bool checkPrime(uint64_t value) {
-  if (value < 2) return false;
+    if (value < 2) {
+        return false;
+    }
     for (uint64_t i = 2; i * i <= value; ++i) {
-        if (value % i == 0) return false;
+        if (value % i == 0) {
+            return false;
+        }
     }
     return true;
 }
@@ -24,7 +28,7 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t num = value + 1;
+    uint64_t num = value + 1;
     while (true) {
         if (checkPrime(num)) {
             return num;
@@ -34,7 +38,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
+    uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; ++i) {
         if (checkPrime(i)) {
             sum += i;
