e0603aea08122c649389ee3fbda64686c4ab076f
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-02-23 14:59:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,38 +11,39 @@ bool checkPrime(uint64_t value) {
 
     for (uint64_t i = 3; i * i <= value; i += 2) {
         if (value % i == 0) return false;
-    }
+      }
     return true;
 }
-
+  
 // Возвращает n-ое простое число
-uint64_t nPrime(uint64_t n) {
+  uint64_t nPrime(uint64_t n) {
     if (n == 0) return 0;
     
     uint64_t count = 0, num = 1;
     while (count < n) {
         num++;
-        if (checkPrime(num)) count++;
-    }
+      if (checkPrime(num)) count++;
+      }
     return num;
 }
-
+  
 // Возвращает ближайшее большее простое число
-uint64_t nextPrime(uint64_t value) {
+  uint64_t nextPrime(uint64_t value) {
     uint64_t num = value + 1;
     while (!checkPrime(num)) {
         num++;
-    }
+      }
     return num;
 }
-
+  
 // Возвращает сумму всех простых чисел до hbound (не включая его)
-uint64_t sumPrime(uint64_t hbound) {
+  uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
             sum += i;
         }
-    }
+      }
     return sum;
 }
+  
