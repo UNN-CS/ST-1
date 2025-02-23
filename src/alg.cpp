0d038a0514b21cf2ae9466c142037b13190a8643
Nanuzer0 <ivragov3@gmail.com>
2025-02-23 19:46:30 +0300

    deleted empty lines

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,6 @@ bool checkPrime(uint64_t value) {
   if (value <= 1) return false;
   if (value <= 3) return true;
   if (value % 2 == 0 || value % 3 == 0) return false;
-  
   for (uint64_t i = 5; i * i <= value; i += 6) {
     if (value % i == 0 || value % (i + 2) == 0)
     return false;
@@ -16,10 +15,8 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
   if (n == 1) return 2;
-  
   uint64_t count = 1;
   uint64_t num = 1;
-  
   while (count < n) {
     num += 2;
     if (checkPrime(num)) {
@@ -39,7 +36,6 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  
   for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i)) {
       sum += i;
